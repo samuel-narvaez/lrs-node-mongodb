ac0131027c1498d203bd567a40f4006bedba7003
Samuel-Abracol <79264880+webdeveloper-abr-samuel@users.noreply.github.com>
2021-05-07 16:51:38 -0500

    fisrt commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const bodyparser = require('body-parser');
+const express = require('express');
+const xapi = require('vtc-lrs');
+const cors = require('cors');
+require('./config/config');
+const app = express();
+
+//middleware xapi
+let options = {
+  lrs: new xapi.LRS(),  
+  connectionString:"mongodb://localhost",
+  getUser:function(req, username, password) {
+    return new xapi.Account(username,true,true);
+  },
+  baseUrl: "http://localhost:3000/xapi"
+};
+app.use("/xapi", new xapi(options) );
+
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors());
+
+
+//Port
+app.listen(process.env.PORT, () => {
+    console.log('Listener in port:', process.env.PORT);
+});
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+////////////////////////////////
+//  PUERTO
+////////////////////////////////
+
+process.env.PORT = process.env.PORT || 4000;
